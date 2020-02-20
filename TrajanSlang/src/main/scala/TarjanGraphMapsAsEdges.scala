a6a8b6e9517416bcc5022bdf2b936d8b1f17012c
Simon Thrane <sthranehansen@gmail.com>
2020-02-20 14:48:02 +0100

    Changed implementation of the experimental implementation to use maps as edges instead of a record type

--- TrajanSlang/src/main/scala/TarjanGraphMapsAsEdges.scala
+++ TrajanSlang/src/main/scala/TarjanGraphMapsAsEdges.scala
@@ -3,14 +3,12 @@
 import org.sireum._
 import org.sireum.ops.{ISZOps, MSZOps}
 
-@datatype class Edge[A](from: A, to: Set[A])
+@record class TarjanGraphMapsAsEdges[A](src: Map[A, Set[A]]) {
 
-@record class TarjanGraphNoMaps[A](src: ISZ[Edge[A]]) {
-
-  @pure def inLoop(fromNode: A, targetNode: A, edges: ISZ[Edge[A]]): B = {
+  @pure def inLoop(fromNode: A, targetNode: A, edges: Map[A, Set[A]]): B = {
     //Connection from node we are searching from to targetNode
-    edges.filter(e => e.from == fromNode && e.to.contains(targetNode)).nonEmpty ||
-      Exists(edges.filter(e => e.from == fromNode).flatMap((x: Edge[A]) => x.to.elements))(newStartNode => inLoop(newStartNode, targetNode, edges))
+    edges.entries.filter((edge) => edge._1 == fromNode && edge._2.contains(targetNode)).nonEmpty ||
+      Exists(edges.entries.filter((edge) => edge._1 == fromNode).flatMap((edge) => edge._2.elements))(newStartNode => inLoop(newStartNode, targetNode, edges))
 
     // Base case There is a direct edge from fromNode to target
     // \E e1, e2 \in nodes * e1 == e2 /\ Edge(e1,e2)
@@ -20,14 +18,14 @@ import org.sireum.ops.{ISZOps, MSZOps}
   }
 
   //Linear over number of veritices and edges: O(V + E)
-  @pure def tarjanAlgo(edges: ISZ[Edge[A]]): MSZ[MSZ[A]] = {
+  @pure def tarjanAlgo(edges: Map[A, Set[A]]): MSZ[MSZ[A]] = {
     Contract(
       Case("No loops in graph",
         Requires(
           edges.nonEmpty &&
             //It is fine to only look at from nodes since all other vertices in graph
             //does not have an outgoing edge
-            !Exists(edges.map(x => x.from))(x => inLoop(x, x, edges))),
+            !Exists(edges.keys)(x => inLoop(x, x, edges))),
         Ensures(
           //A list inside the nested result list should have a length of one (No SCC)
           All(Res[MSZ[MSZ[A]]])(l => l.size == 1) &&
@@ -41,7 +39,7 @@ import org.sireum.ops.{ISZOps, MSZOps}
         Requires(
           edges.nonEmpty &&
             //There should exist at least one node that is a part of a loop<
-            Exists(edges.map(x => x.from))(x => inLoop(x, x, edges))),
+            Exists(edges.keys)(x => inLoop(x, x, edges))),
         Ensures(
           //All elements in a nested list (SCC) with a length >= 2 should be a part of a loop
           All(Res[MSZ[MSZ[A]]].filter(x => x.size >= 2).flatMap((x: MSZ[A]) => x))(e => inLoop(e, e, edges)) &&
@@ -68,7 +66,7 @@ import org.sireum.ops.{ISZOps, MSZOps}
       //Add to stack
       s = s :+ v
 
-      edges.filter(o => o.from == v).flatMap(o => o.to.elements).foreach(w => {
+      edges.entries.filter(o => o._1 == v).flatMap(o => o._2.elements).foreach(w => {
         if (!index.contains(w)) {
           //Perform DFS from node W, if node w is not explored yet
           visit(w)
@@ -93,7 +91,7 @@ import org.sireum.ops.{ISZOps, MSZOps}
     }
 
     //Perform a DFS from  all no nodes that hasn't been explored
-    src.foreach(v => if (!index.contains(v.from)) visit(v.from))
+    edges.keys.foreach(v => if (!index.contains(v)) visit(v))
     return ret
   }
 
@@ -104,7 +102,7 @@ import org.sireum.ops.{ISZOps, MSZOps}
         //Index should contain the two keys
         index.contains(v) && index.contains(w) &&
           //All values of the range of index should be a valid index for lowlink
-        All(index.values)((rangeVal:Z) => ISZOps(lowLink.indices).contains(rangeVal))),
+          All(index.values)((rangeVal: Z) => ISZOps(lowLink.indices).contains(rangeVal))),
       Ensures(Res[Z] <= lowLink(index.get(w).get) && Res[Z] <= lowLink(index.get(v).get))
     )
     if (lowLink(index.get(v).get) > lowLink(index.get(w).get)) {
@@ -114,35 +112,35 @@ import org.sireum.ops.{ISZOps, MSZOps}
     }
   }
 
-  @strictpure def nodesInSystem(edges: ISZ[Edge[A]]): Z =
-    (Set.empty[A] ++ edges.map((x: Edge[A]) => x.from) ++ edges.flatMap((x: Edge[A]) => x.to.elements)).size
+  @strictpure def nodesInSystem(edges: Map[A, Set[A]]): Z =
+    (Set.empty[A] ++ edges.keys ++ edges.values.flatMap(x => x.elements)).size
 
   val tarjan: MSZ[MSZ[A]] = tarjanAlgo(src)
 
   val hasCycle: B = tarjan.elements.exists(c => c.size >= 2)
   val tarjanCycle: Seq[MSZ[A]] = tarjan.elements.filter(c => c.size >= 2).distinct.map(c => c)
 
-  def topologicalSort(edges: ISZ[Edge[A]]): ISZ[A] = {
+  def topologicalSort(edges: Map[A, Set[A]]): ISZ[A] = {
     Contract(
       Case("Graph contains cycles so a topological ordering cannot be found. The function shall instead return a list with all the SCC/loops",
         Requires(
           edges.nonEmpty &&
-            Exists(edges.map(x => x.from))(x => inLoop(x, x, edges))),
-        Ensures(Res[ISZ[A]].isEmpty)
-      ),
-      Case("Graphs contains no loops",
-        Requires(
-          edges.nonEmpty &&
-            !Exists(edges.map(x => x.from))(x => inLoop(x, x, edges))),
-        Ensures(
-          //There should be no element at an index > n, that has a connection to a component placed at index n
-          All(Res[ISZ[A]].indices)(n => !Exists(Res[ISZ[A]].indices)(j => j > n && inLoop(Res[ISZ[A]](j), Res[ISZ[A]](n), edges))) &&
-            //All elements/vertices in the graph should be in the resulting list
-            Res[MSZ[MSZ[A]]].size == nodesInSystem(edges)
+            Exists(edges.keys)(x => inLoop(x, x, edges))),
+          Ensures(Res[ISZ[A]].isEmpty)
+        ),
+        Case("Graphs contains no loops",
+          Requires(
+            edges.nonEmpty &&
+              !Exists(edges.keys)(x => inLoop(x, x, edges))),
+          Ensures(
+            //There should be no element at an index > n, that has a connection to a component placed at index n
+            All(Res[ISZ[A]].indices)(n => !Exists(Res[ISZ[A]].indices)(j => j > n && inLoop(Res[ISZ[A]](j), Res[ISZ[A]](n), edges))) &&
+              //All elements/vertices in the graph should be in the resulting list
+              Res[MSZ[MSZ[A]]].size == nodesInSystem(edges)
+          )
         )
       )
-    )
-    if (hasCycle) {
+    if (hasCycle){
       return ISZ[A]()
     }
     else {
